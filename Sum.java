2928dbdee731e36acd53c1c02b0457e3f45b9828
Vijay Chauhan <vjchauhan861@gmail.com>
2023-01-06 11:50:45 +0530

    added message in Sum.java

--- Sum.java
+++ Sum.java
@@ -5,6 +5,6 @@ public class Sum{
         int a = 5;
         int b = 6;
         int sum = a+b;
-        System.out.println(sum);
+        System.out.println("sum of "+ a + b +" is : "+sum);
     }
 }
